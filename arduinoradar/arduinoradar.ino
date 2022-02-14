1bd42c65279be224caf66f24d66b3e399b6decbd
Orsino Francesco <orsinofrancesco06@gmail.com>
2022-02-14 12:10:08 +0100

    arduino

--- arduinoradar/arduinoradar.ino
+++ arduinoradar/arduinoradar.ino
@@ -1,9 +1,21 @@
+#define mindeg 0
+#define maxdeg 90
+#define stepdeg 1
+int state = 1;
+int deg = mindeg;
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("radar");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(deg);
+  deg = deg + state;
+  if(deg == maxdeg){
+      state = 0-stepdeg;
+  }
+  if(deg == mindeg){
+      state = stepdeg;
+  }
 
 }
